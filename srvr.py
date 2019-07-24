150a02ba6d077fd229ca44a2d912c603842d6d1a
seethalprince <seethalprincee@gmail.com>
2019-07-24 10:25:28 +0530

    if

--- srvr.py
+++ srvr.py
@@ -26,9 +26,25 @@ class App:
         bl = json_obj['bl']
         
         #do something with the data
+	if(hr>=91 and hr<=100):
+            print("excelent")
+        elif(hr>=81 and hr<=90):
+            print("good")
+        elif(hr>=71 and hr<=80):
+            print("normal")
+        elif(hr>=61 and hr<=70):
+            print("may be")
+        elif(hr>=51 and hr<=60):
+            print("less")
+        elif(hr>=41 and hr<=50):
+            print("poor")
+        elif(hr>=0 and hr<=40):
+            print(".....")
+        else:
+            print("Strange ..!!")
 
-        rslt= 20
-        return {"exists": rslt}
+        #rslt= 20
+        #return {"exists": rslt}
 
             
 if __name__ == '__main__':
