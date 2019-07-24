33409d318cf03c1b45c7579d340c2fa9a05331ac
digitalsnipers <muhammedshameelkt@gmail.com>
2019-07-24 12:44:38 +0530

    final

--- srvr.py
+++ srvr.py
@@ -9,6 +9,26 @@ import cherrypy
 
 
 class App:
+    
+    def status(self,hr):
+        if(hr>=91 and hr<=100):
+            return("excelent")
+        elif(hr>=81 and hr<=90):
+            return("good")
+        elif(hr>=71 and hr<=80):
+            return("normal")
+        elif(hr>=61 and hr<=70):
+            return("may be")
+        elif(hr>=51 and hr<=60):
+            return("less")
+        elif(hr>=41 and hr<=50):
+            return("poor")
+        elif(hr>=0 and hr<=40):
+            return(".....")
+        else:
+            return("Strange ..!!")
+    
+
     @cherrypy.expose
     def index(self):
         return open('home.html')
@@ -24,27 +44,15 @@ class App:
         bt = json_obj['bt']
         bp = json_obj['bp']
         bl = json_obj['bl']
+
+        
         
         #do something with the data
-	if(hr>=91 and hr<=100):
-            print("excelent")
-        elif(hr>=81 and hr<=90):
-            print("good")
-        elif(hr>=71 and hr<=80):
-            print("normal")
-        elif(hr>=61 and hr<=70):
-            print("may be")
-        elif(hr>=51 and hr<=60):
-            print("less")
-        elif(hr>=41 and hr<=50):
-            print("poor")
-        elif(hr>=0 and hr<=40):
-            print(".....")
-        else:
-            print("Strange ..!!")
-
-        #rslt= 20
-        #return {"exists": rslt}
+        
+	    
+        value=int(hr)
+        
+        return {"status": self.status(value)}
 
             
 if __name__ == '__main__':
